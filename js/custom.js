119703ebaf765683d67e831dc16069a056294d9f
darkside <andrey.garbuz@gmail.com>
2015-04-05 20:39:31 +0300

    Add logo. Remove unused assets

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,18 @@
-/*Theme    : assan
- * Author  : Design_mylife
- * Version : V1.4
- *
- */
-
-
-//sticky header on scroll
-$(document).ready(function () {
-    $(window).load(function () {
-        $(".sticky").sticky({topSpacing: 0});
-    });
-});
 
 /* ==============================================
  WOW plugin triggers animate.css on scroll
  =============================================== */
-$(document).ready(function () {
-    var wow = new WOW(
-            {
-                boxClass: 'wow', // animated element css class (default is wow)
-                animateClass: 'animated', // animation css class (default is animated)
-                offset: 100, // distance to the element when triggering the animation (default is 0)
-                mobile: false        // trigger animations on mobile devices (true is default)
-            }
-    );
-    wow.init();
-});
+// $(document).ready(function () {
+//     var wow = new WOW(
+//             {
+//                 boxClass: 'wow', // animated element css class (default is wow)
+//                 animateClass: 'animated', // animation css class (default is animated)
+//                 offset: 100, // distance to the element when triggering the animation (default is 0)
+//                 mobile: false        // trigger animations on mobile devices (true is default)
+//             }
+//     );
+//     wow.init();
+// });
 
 
 //owl carousel for work
@@ -48,22 +35,17 @@ $(document).ready(function () {
 // });
 
 
-//owl carousel for news
-/*========tooltip and popovers====*/
-$(document).ready(function () {
-$("[data-toggle=popover]").popover();
-
-$("[data-toggle=tooltip]").tooltip();
+$(function () {
+  $("[data-toggle=popover]").popover();
+  $("[data-toggle=tooltip]").tooltip();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#logoImage').attr('src', '/img/logo_small.png');
+      $('#main-nav').addClass('sticky');
+    } else {
+      $('#logoImage').attr('src', '/img/logo.png');
+      $('#main-nav').removeClass('sticky');
+    }
+  });
 });
 
-//transparent header
-
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.transparent-header').css("background", "#252525");
-        } else {
-            $('.transparent-header').css("background", "transparent");
-        }
-    });
-});
